ea3e92dd0fef5b84e360b21fde9efc34f95aaeb4
th293 <54948144+th293@users.noreply.github.com>
2019-09-17 11:19:15 -0400

    Update initDB.php

--- initDB.php
+++ initDB.php
@@ -9,6 +9,15 @@ $connection_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
 	$db = new PDO($connection_string, $username, $password);
 	echo "Should have connected";
+	$query = "create table if not exists `TestUsers`(
+		`id` int auto_increment not null,
+		`username` varchar(30) not null unique,
+		`pin` int default 0,
+		PRIMARY KEY (`id`)
+		) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	echo "<br>" . $r . "<br>";
 }
 catch(Exception $e){
 	echo $e->getMessage();
