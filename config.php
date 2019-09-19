2f0ed3834f46c5efa5dfb90fa59b2253890adbe6
th293 <th293@njit.edu>
2019-09-19 15:47:42 -0400

    adding initDB.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host="";
-$database="";
-$username="";
-$password="";
-?>
---
+++ initDB.php
@@ -0,0 +1,9 @@
+<?php
+//TODO add error handling
+
+//load the config from the same directory
+require('config.php');
+echo "Loaded host: " . $host;
+
+?>
+
