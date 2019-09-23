37e0b5adcefb259828c636acb797118bd43debb4
th293 <th293@njit.edu>
2019-09-23 19:39:02 -0400

    adding initDB.php

--- initDB.php
+++ initDB.php
@@ -5,4 +5,14 @@
 require('config.php');
 echo "Loaded host: " . $host;
 
+try{
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+	$db = new PDO($conn_string, $username, $password);
+		echo "Connected";
+		}
+		catch(Exception $e){
+			echo $e->getMessage();
+				echo "Something went wrong";
+				}
+
 ?>
