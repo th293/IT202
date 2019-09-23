524ba69d543fdc10c563d8402439f13a4cc4a119
th293 <th293@njit.edu>
2019-09-23 14:02:07 -0400

    adding initDB.php

---
+++ initDB.php
@@ -0,0 +1,22 @@
+<?php
+//TODO add error handling
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//load the config from the same directory
+require('config.php');
+echo "Loaded host: " . $host;
+
+//new lines below
+try{
+	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+		$db = new PDO($conn_string, $username, $password);
+			echo "Connected";
+			}
+			catch(Exception $e){
+				echo $e->getMessage();
+					echo "Something went wrong";
+					}
+
+?>
