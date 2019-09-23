3a9c1917285c8e40fcc17ad996c799c76b0d188f
th293 <th293@njit.edu>
2019-09-23 19:41:13 -0400

    adding initDB.php

--- initDB.php
+++ initDB.php
@@ -1,6 +1,8 @@
 <?php
 //TODO add error handling
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 //load the config from the same directory
 require('config.php');
 echo "Loaded host: " . $host;
