7796bedc79e723a1b56386a433e97804bd553216
th293 <th293@njit.edu>
2019-11-01 23:38:38 -0400

    adding milestone.php

--- project/sql/customer_table.sql
+++
@@ -1,9 +0,0 @@
-
-//Creating Table here
-        $sql_table = "CREATE TABLE Customer";
-        `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        `Username` VARCHAR(30) NOT NULL UNIQUE,
-        `Password` int default 0,
-         reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-         CHARACTER SET utf8 COLLATE utf8_general_ci";
-
